503fbedfdeb325040a2e250baf7e797e1919a131
Felipe Jurado <felipejm9805@gmail.com>
2020-03-25 12:59:39 -0500

    Comments on SD-Parcial directory

--- SD-Parcial/topic-receive-sd.py
+++ SD-Parcial/topic-receive-sd.py
@@ -2,25 +2,32 @@
 import pika
 import sys
 
+#Las credenciales para conectarnos al servidor RabbitMQ
 credentials = pika.PlainCredentials('root','password')
 
+#Conexion con nuestro servidor RabbitMQ
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('192.168.0.10', credentials=credentials))
 channel = connection.channel()
 
+#Declaramos el exchange
 channel.exchange_declare(exchange='topic-messages', exchange_type='topic')
 
+#Declaramos la cola, y la etiqueta exclusive, nos indica
+#que una vez la conexion del consumidor se cierra, la cola 
+#sera eliminada
 result = channel.queue_declare('', exclusive=True)
 queue_name = result.method.queue
 
+#El binding_key nos permite decirle al exchange que envie los mensajes
+#a nuestra cola
 binding_keys = sys.argv[1:]
 if not binding_keys:
     sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
     sys.exit(1)
 
 for binding_key in binding_keys:
-    channel.queue_bind(
-        exchange='topic-messages', queue=queue_name, routing_key=binding_key)
+    channel.queue_bind(exchange='topic-messages', queue=queue_name, routing_key=binding_key)
 
 print(' [*] Esperando mensajes. Para salir presione CTRL +C')
 
